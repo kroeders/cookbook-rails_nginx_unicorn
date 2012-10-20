5525663f9a00911249410f14aa87d3727fb28ab2
Aaron Jensen <aaronjensen@gmail.com>
2012-10-19 21:12:53 -0700

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Substantial"
+maintainer_email "aaron@substantial.com"
+license          "MIT"
+description      "Installs/Configures rails-nginx-unicorn"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
