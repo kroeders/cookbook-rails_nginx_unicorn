8bf2145b03e55a3e79598dc04516409acfbd3e9a
Aaron Jensen <aaronjensen@gmail.com>
2012-10-20 00:05:10 -0700

    Add support to enable or disable site

--- providers/app.rb
+++ providers/app.rb
@@ -2,10 +2,16 @@ action :create do
   common = {
     :name => new_resource.name,
     :server_names => new_resource.server_names,
-    :app_root => new_resource.app_root
+    :app_root => new_resource.app_root,
+    :enabled => new_resource.enabled
   }
 
   directory new_resource.app_root do
+    owner node[:rails_nginx_unicorn][:deploy_user]
+    recursive true
+  end
+
+  directory "#{new_resource.app_root}/shared/log" do
     owner node[:rails_nginx_unicorn][:deploy_user]
     recursive true
   end
@@ -16,6 +22,10 @@ action :create do
     cookbook "rails_nginx_unicorn"
     variables common
     notifies :reload, "service[nginx]"
+  end
+
+  nginx_site "#{new_resource.name}.conf" do
+    enable common[:enabled]
   end
 end
 
--- resources/app.rb
+++ resources/app.rb
@@ -5,3 +5,4 @@ default_action :create
 attribute :name, :kind_of => String, :name_attribute => true
 attribute :server_names, :kind_of => String, :default => "localhost"
 attribute :app_root, :kind_of => String, :required => true
+attribute :enabled, :default => true
