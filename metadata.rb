68d770f01c491285212cccd55cd5f8153e08f9f7
Aaron Jensen <aaronjensen@gmail.com>
2012-10-20 17:39:29 -0700

    remove dependency on unicorn

--- metadata.rb
+++ metadata.rb
@@ -5,4 +5,3 @@ description      "Installs/Configures rails_nginx_unicorn"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 depends          "nginx"
-depends          "unicorn"
