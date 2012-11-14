96cb7df8137dcd820da520718fb802b9dd079698
Adam Smith <re5etsmyth@gmail.com>
2012-11-14 13:44:46 -0800

    Set asset root attribute

--- resources/app.rb
+++ resources/app.rb
@@ -6,6 +6,7 @@ attribute :name, :kind_of => String, :name_attribute => true
 attribute :server_names, :kind_of => String, :default => "localhost"
 attribute :server_port, :kind_of => Integer, :default => 80
 attribute :app_root, :kind_of => String, :required => true
+attribute :asset_root, :kind_of => String, :required => false
 attribute :enabled, :default => true
 attribute :deploy_user, :kind_of => String, :default => 'deploy'
 attribute :deploy_group, :kind_of => String, :default => 'deploy'
