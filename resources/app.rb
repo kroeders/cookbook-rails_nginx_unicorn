72007ae8c3ea2a82dc7e6f8427da241c41f6efa6
Greg Musick <greg@substantial.com>
2013-10-02 11:46:20 -0700

    Redirect to https if force_ssl is true

--- resources/app.rb
+++ resources/app.rb
@@ -14,6 +14,7 @@ attribute :deploy_user, :kind_of => String, :default => 'deploy'
 attribute :deploy_group, :kind_of => String, :default => 'deploy'
 attribute :rvm_ruby_string, :kind_of => String
 attribute :rails_env, :kind_of => String, :default => 'production'
+attribute :force_ssl, :default => false
 attribute :ssl, :default => false
 attribute :ssl_certificate_path, :kind_of => String, :default => '/usr/local/nginx/conf/cert.pem'
 attribute :ssl_certificate_key_path, :kind_of => String, :default => '/usr/local/nginx/conf/cert.key'
