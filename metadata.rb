33fb3c91a25864d5a3d7ae5137f1f6cea3d2cef9
keefe roedersheimer <keefe.roedersheimer@bluescape.com>
2014-12-28 16:28:37 -0800

    Update metadata.rb
    to make chef12 happy

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,4 @@ description      "Installs/Configures rails_nginx_unicorn"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 depends          "nginx"
+name            "rails-nginx-unicorn"
